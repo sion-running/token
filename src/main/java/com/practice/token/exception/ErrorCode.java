122e0e3844daf81ad6069b112480d1d8360cb9d0
sion-running <staythismoment.sion@gmail.com>
2024-09-28 15:25:07 +0900

    feat: 토큰 만료를 나타내는 에러코드 추가

--- src/main/java/com/practice/token/exception/ErrorCode.java
+++ src/main/java/com/practice/token/exception/ErrorCode.java
@@ -11,6 +11,7 @@ public enum ErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, null),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, null),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, null),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, null),
     ;
 
     private final HttpStatus status;
