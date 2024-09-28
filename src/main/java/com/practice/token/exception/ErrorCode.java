0bbee2260b105f0047eafa46ce1269b1e15918a0
sion-running <staythismoment.sion@gmail.com>
2024-09-28 14:18:40 +0900

    feat: 유효하지 않은 토큰 관련 새로운 에러코드 정의

--- src/main/java/com/practice/token/exception/ErrorCode.java
+++ src/main/java/com/practice/token/exception/ErrorCode.java
@@ -10,6 +10,7 @@ public enum ErrorCode {
     DUPLICATE_USER_NAME(HttpStatus.CONFLICT, "Duplicate user name"),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, null),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, null),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, null),
     ;
 
     private final HttpStatus status;
