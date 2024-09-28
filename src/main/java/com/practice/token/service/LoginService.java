d4f9f9de1473f4dd79d62d155d0c992d6fd8d841
sion-running <staythismoment.sion@gmail.com>
2024-09-28 14:57:43 +0900

    refactor: 로그인 서비스에서 유저 확인 시 user repository가 아닌 service의 메소드를 호출하도록 변경

--- src/main/java/com/practice/token/service/LoginService.java
+++ src/main/java/com/practice/token/service/LoginService.java
@@ -5,7 +5,6 @@ import com.practice.token.exception.ApplicationException;
 import com.practice.token.exception.ErrorCode;
 import com.practice.token.model.dto.TokenDto;
 import com.practice.token.model.entity.User;
-import com.practice.token.repository.UserRepository;
 import com.practice.token.util.JwtTokenUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,19 +13,19 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class LoginService {
-    private final UserRepository userRepository;
+    private final UserService userService;
     private final PasswordEncoder passwordEncoder;
     private final JwtTokenUtil jwtTokenUtil;
 
     public TokenDto login(LoginRequest request) {
-        User user = userRepository.findByUserName(request.getUserName()).orElseThrow(() -> new ApplicationException(ErrorCode.USER_NOT_FOUND));
+        User user = userService.findUserOrElseThrow(request.getUserName());
 
         if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
             throw new ApplicationException(ErrorCode.INVALID_PASSWORD);
         }
 
         return TokenDto.builder()
-                .accessToken(jwtTokenUtil.generateAccessToken(user.getUserName()))
+                .accessToken(jwtTokenUtil.generateAccessToken(user.getUsername()))
                 .refreshToken(jwtTokenUtil.generateRefreshToken())
                 .build();
     }
